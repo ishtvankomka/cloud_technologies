e027f4c444fc155f7c2261c454ab0afb4d07e576
“ishtvankomka” <ishtvan.komonii.it.2021@lpnu.ua>
2023-06-07 01:39:50 +0300

    Add: lab 4 basic structure

---
+++ lab4/main.tf
@@ -0,0 +1,33 @@
+module "slack_lambda" {
+  source     = "../modules/lambda-slack"
+  handler    = "index.handler"
+  lambda_zip = "${path.module}/code.zip"
+  context    = module.naming.context
+
+  env_var = {
+    SLACK_URL = "<slack url>"
+  }
+
+  depends_on = [null_resource.create_lambda]
+}
+
+module "billng_alert" {
+  source                    = "../modules/billing_alerts"
+  context                   = module.naming.context
+  monthly_billing_threshold = "200"
+  currency                  = "USD"
+  aws_account_id            = "073240252659"
+}
+
+resource "null_resource" "create_lambda" {
+  provisioner "local-exec" {
+    command = "cd src && zip -r ../code.zip ."
+  }
+}
+
+resource "aws_sns_topic_subscription" "sns_to_slack" {
+  topic_arn  = module.billng_alert.sns_topic_arn[0]
+  protocol   = "lambda"
+  endpoint   = module.slack_lambda.lambda_arn
+  depends_on = [module.billng_alert, module.slack_lambda]
+}
